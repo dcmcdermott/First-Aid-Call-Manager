f7f7845f6dd7b28638ca39db3e26503972ce3fad
dcmcdermott <dc.mcdermott22@gmail.com>
2022-11-27 14:46:52 -0500

    Filters update
    added search filters for Date to Calls and Walkins. Reordered filter fields

--- calls/filters.py
+++ calls/filters.py
@@ -1,31 +1,41 @@
 import django_filters
-from django_filters import CharFilter
+from django_filters import CharFilter, DateFilter, BooleanFilter, ChoiceFilter
+from django.forms import DateTimeInput, CheckboxInput
 
 from .models import *
 
 
 class ResponderFilter(django_filters.FilterSet):
 
+    STATUS = (
+        ('PT', 'PT'),
+        ('PT-S', 'PT-S'),
+        ('FT-S', 'FT-S')
+    ) 
+
+    ft_pt = ChoiceFilter(field_name="status", choices=STATUS, label= 'FT/PT')
+
     class Meta:
         model = Responder
-        fields = ['lastname', 'status', 'certification']
+        fields = ['lastname', 'certification', 'ft_pt']
 
 
 class CallFilter(django_filters.FilterSet):
 
     location = CharFilter(field_name="location", lookup_expr="icontains")
+    call_date = DateFilter(field_name="datetime__date", label= 'Date', widget=DateTimeInput(attrs={'type': 'date'}))
+    red = BooleanFilter(field_name="red", label= 'Red', widget=CheckboxInput())
 
     class Meta:
         model = Call
-        fields = ['nature', 'responder', 'red']
+        fields = ['call_date','nature', 'responder', 'red']
 
 
 class WalkinFilter(django_filters.FilterSet):
 
-    reason = CharFilter(field_name="reason", lookup_expr="icontains")
-    notes = CharFilter(field_name="notes", lookup_expr="icontains")
+    reason = CharFilter(field_name="reason", label= 'Reason', lookup_expr="icontains")
+    walkin_date = DateFilter(field_name="datetime__date", label= 'Date', widget=DateTimeInput(attrs={'type': 'date'}))
     
     class Meta:
         model = Walkin
-        fields = '__all__'
-        exclude = ['datetime', 'firstname', 'notes', 'reason']
+        fields = ['walkin_date', 'lastname', 'department', 'reason']
