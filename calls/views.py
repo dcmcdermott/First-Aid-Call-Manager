0eba51cd146598ff777b261a296475537bf86084
dcmcdermott <dc.mcdermott22@gmail.com>
2022-11-29 23:01:17 -0500

    Status boxes 1
    consolidated the info from the status boxes into one box in preparation for statuses

--- calls/views.py
+++ calls/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.utils.timezone import datetime
 from django.core.paginator import Paginator
 
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
